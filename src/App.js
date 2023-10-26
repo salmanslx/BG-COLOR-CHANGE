b0f8f739e0ff67649b7bad98f79841966058f5fe
salmanslx <salmanulfaris712@gmail.com>
2023-10-26 20:35:06 +0530

    V1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [background,setBackground] = useState("light")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+      <div style={{height:'100vh'}} className='text-center d-flex justify-content-center flex-column align-items-center bg-dark'>
+        <h1 className='text-light'>Color Changer</h1>
+        <div style={{width:'600px',height:'350px'}}  className={`d-flex justify-content-center align-items-center bg-${background} rounded`}>
+          <div>
+            <button onClick={()=>setBackground("danger")} className='bg-danger btn border ms-2'>RED</button>
+            <button onClick={()=>setBackground("success")} className='bg-success btn border ms-2'>GREEN</button>
+            <button onClick={()=>setBackground("primary")} className='bg-primary btn border ms-2'>BLUE</button>
+            <button onClick={()=>setBackground("warning")} className='bg-warning btn border ms-2'>YELLOW</button>
+            <button onClick={()=>setBackground("light")} className='bg-dark btn border text-light ms-2'>RESET</button>
+          </div>
+        </div>
+      </div>
+   </>
   );
 }
 
